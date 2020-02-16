018b5e475a3f9d106168ef56f5a215a88dfd620d
Diego França <diego.tg.franca@gmail.com>
2020-02-16 19:52:25 -0300

    adicionando confiugracoes do travis.yml

--- server.js
+++ server.js
@@ -8,15 +8,24 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', message);
   });
   // criando um broadcast
- wss.clients.forEach((cliente)=>{
-     if( cliente.readyState ===WebSocket.OPEN){
-      iostat().on('data', function(err, stats) {
-         console.log(stats)
-         cliente.send(JSON.stringify(stats));
-         
-       })
-     }
-  })
- 
+
+  /*iostat().on('data', function(err, stats) {
+   console.log(stats)
+   ws.send(JSON.stringify(stats));
+   
+ })*/
  
 });
+
+const broadcast = ()=>{
+   wss.clients.forEach((cliente)=>{
+      if( cliente.readyState ===WebSocket.OPEN){
+       iostat().on('data', function(err, stats) {
+          console.log(stats)
+          cliente.send(JSON.stringify(stats));
+          
+        })
+      }
+   })
+}
+setInterval(broadcast, 3000)
